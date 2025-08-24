246491f5241154a91e35049d0449d6ce96a90740
BandhanPramanik <bandhanpramanik06.foss@gmail.com>
2025-08-24 13:19:04 +0530

    Indentation correction

--- memory.py
+++ memory.py
@@ -6,31 +6,33 @@ import shlex
 filename = "a.out"
 command = "xyz"
 
-command_repo = {
-    "load": lambda f: gdbmi.write(f"-file-exec-file {shlex.quote(f)}"),
-    "breakpoint": lambda line: gdbmi.write(f"-break-insert {safe_line(line)}"),
-    "run": lambda: gdbmi.write("-exec-run"),
-    "next": lambda: gdbmi.write("-exec-step"),
-    "list-frames": lambda: gdbmi.write("-stack-list-frames"),
-    "eval": lambda expr: gdbmi.write(f"-data-evaluate-expression {shlex.quote(expr)}"),
-    "exit": lambda: gdbmi.write("-gdb-exit")
-}
-
-
-# Start gdb process
-gdbmi = GdbController()
-print(gdbmi.get_subprocess_cmd())  # print actual command run as subprocess
-
-# Load binary a.out and get structured response
-response = ;
-pprint(response)
-
-
-
-
 def safe_line(line_str):
     if line_str.isdigit():
         return int(line_str)
     else:
         raise ValueError("Invalid line number")
+
+def exec_debug(command):
+    command_repo = {
+        "load": lambda f: gdbmi.write(f"-file-exec-file {shlex.quote(f)}"),
+        "breakpoint": lambda line: gdbmi.write(f"-break-insert {safe_line(line)}"),
+        "run": lambda: gdbmi.write("-exec-run"),
+        "next": lambda: gdbmi.write("-exec-step"),
+        "list-frames": lambda: gdbmi.write("-stack-list-frames"),
+        "eval": lambda expr: gdbmi.write(f"-data-evaluate-expression {shlex.quote(expr)}"),
+        "exit": lambda: gdbmi.write("-gdb-exit")
+    }
+
+
+    # Start gdb process
+    gdbmi = GdbController()
+    print(gdbmi.get_subprocess_cmd())  # print actual command run as subprocess
+
+    # Load binary a.out and get structured response
+    response = ;
+    pprint(response)
+
+
+
+
 
